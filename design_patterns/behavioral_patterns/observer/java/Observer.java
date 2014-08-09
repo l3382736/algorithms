a6a310cab0b0fd6c703788e04dee72b5a54079a8
eduardo leon <eduardoleonrodriguez@gmail.com>
2014-08-09 03:31:47 +0200

    design pattern observer

---
+++ design_patterns/behavioral_patterns/observer/java/Observer.java
@@ -0,0 +1,10 @@
+public class Observer {
+    public static void main(String args[]) {
+        System.out.println("Enter text >");
+        final EventSource eventSource = new EventSource();
+        final ResponseHandler responseHandler = new ResponseHandler();
+        eventSource.addObserver(responseHandler);
+        Thread thread = new Thread(eventSource);
+        thread.start();
+    }
+}
